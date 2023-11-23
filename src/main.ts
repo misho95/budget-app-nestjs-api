94b9fab914f2e805d1a0728b975b4b3755827909
Misho <chapidze.misho95@gmail.com>
2023-11-23 13:57:39 +0400

    s

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
 }
 bootstrap();
