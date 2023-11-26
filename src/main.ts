b401506982ab90c411f5d6d07699edef566e21a0
Misho <chapidze.misho95@gmail.com>
2023-11-26 11:09:45 +0400

    sx

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 8080;
