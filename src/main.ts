3a60ca0dc4ad89762d889c0ad1df49a366a85f24
Misho <chapidze.misho95@gmail.com>
2023-12-08 14:00:21 +0400

    fxd

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
