081120886afa38e28ce2c299b7d59a71027b5293
Misho <chapidze.misho95@gmail.com>
2023-12-10 11:05:02 +0400

    fxd

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtModule } from "@nestjs/jwt";
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: "1d" },
+      signOptions: { expiresIn: "1h" },
     }),
   ],
   controllers: [AuthController],
